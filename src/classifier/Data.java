8cdb0af7cc208c74f17d3d5308d1adb1bc4f96b1
DennisdeW <dennisdw@outlook.com>
2014-11-24 09:34:54 +0100

    Comments

--- src/classifier/Data.java
+++ src/classifier/Data.java
@@ -19,18 +19,27 @@ public class Data {
 	}
 
 	public void tokenize() {
+		// Get a list of all training files
 		List<File> files = Arrays.asList(dir.listFiles());
 		for (File f : files)
 			try {
+				// Read the file to a string
 				FileInputStream fis = new FileInputStream(f);
 				byte[] raw = new byte[(int) f.length()];
 				fis.read(raw);
 				fis.close();
 				String fullFile = new String(raw);
+
+				// Split at whitespace
 				List<String> tokens = Arrays.asList(fullFile.split("\\s+"));
+
 				List<String> normalised = new ArrayList<>();
 				for (String s : tokens)
+					// Normalise by removing non-word and non-digit characters,
+					// and by converting to lower case
 					normalised.add(s.replaceAll("[^\\w\\d]", "").toLowerCase());
+				
+				//add to the set of normalised tokens
 				data.add(normalised);
 			} catch (Exception e) {
 				e.printStackTrace();
